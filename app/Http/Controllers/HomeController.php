fa3b287c603eb5bbeaa6d2c24f1702d3fdcf9d67
Liam Nelson <lmjnelson@yahoo.com>
2018-03-28 17:24:36 +0100

    Authentication scaffold

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        if(Auth::guest()){
+            return view('welcome');
+        }
+
+        return view('home');
+    }
+}
